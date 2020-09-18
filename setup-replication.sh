20ba31bb5f88b5ce428f86ab16014d982b5ee9a1
Meo CHuot <diep.pham@amela.vn>
2020-09-18 17:28:13 +0700

    fix replation for pg12

--- setup-replication.sh
+++ setup-replication.sh
@@ -37,12 +37,12 @@ EOF
 
 else
 
-cat > ${PGDATA}/recovery.conf <<EOF
+cat > ${PGDATA}/postgresql.auto.conf <<EOF
 standby_mode = on
 primary_conninfo = 'host=${REPLICATE_FROM} port=5432 user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}'
 trigger_file = '/tmp/touch_me_to_promote_to_me_master'
 EOF
-chown postgres ${PGDATA}/recovery.conf
-chmod 600 ${PGDATA}/recovery.conf
+chown postgres ${PGDATA}/postgresql.auto.conf
+chmod 600 ${PGDATA}/postgresql.auto.conf
 
 fi
