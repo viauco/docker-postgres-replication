a7905351b57c6ca42711e1eabfe57df959567bf2
Meo CHuot <diep.pham@amela.vn>
2020-09-18 16:57:11 +0700

    init

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM postgres:latest
+LABEL maintainer="Viauco"
+ENV PG_MAX_WAL_SENDERS 8
+ENV PG_WAL_KEEP_SEGMENTS 8
+COPY setup-replication.sh /docker-entrypoint-initdb.d/
+COPY docker-entrypoint* /usr/local/bin/
+RUN chmod +x /docker-entrypoint-initdb.d/setup-replication.sh /docker-entrypoint.sh \
+    && /usr/local/bin/docker-entrypoint-patcher.sh
+
---
+++ setup-replication.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -e
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'POSTGRES_USER' 'postgres'
+file_env 'POSTGRES_PASSWORD'
+file_env 'REPLICATE_FROM'
+
+if [ "x$REPLICATE_FROM" == "x" ]; then
+
+cat >> ${PGDATA}/postgresql.conf <<EOF
+wal_level = hot_standby
+max_wal_senders = $PG_MAX_WAL_SENDERS
+wal_keep_segments = $PG_WAL_KEEP_SEGMENTS
+hot_standby = on
+EOF
+
+else
+
+cat > ${PGDATA}/recovery.conf <<EOF
+standby_mode = on
+primary_conninfo = 'host=${REPLICATE_FROM} port=5432 user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}'
+trigger_file = '/tmp/touch_me_to_promote_to_me_master'
+EOF
+chown postgres ${PGDATA}/recovery.conf
+chmod 600 ${PGDATA}/recovery.conf
+
+fi
